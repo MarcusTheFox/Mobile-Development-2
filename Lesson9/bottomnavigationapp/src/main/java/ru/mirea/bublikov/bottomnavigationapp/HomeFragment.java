b96c7810feb0bd479f24387d2705f270c0e96ed7
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-12-20 01:22:15 +0300

    7: BottomNavigationApp complete

---
+++ Lesson9/bottomnavigationapp/src/main/java/ru/mirea/bublikov/bottomnavigationapp/HomeFragment.java
@@ -0,0 +1,21 @@
+package ru.mirea.bublikov.bottomnavigationapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import ru.mirea.bublikov.bottomnavigationapp.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
